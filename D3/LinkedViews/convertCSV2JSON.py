1b5aa4cde46fdb1ed7f7780536791fefe30c42b7
MirjaLagerwaard <mirja_1994@hotmail.com>
2016-12-13 17:14:49 +0100

    Finished the map and barchart

---
+++ D3/LinkedViews/convertCSV2JSON.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# Name: Mirja Lagerwaard
+# Student number: 10363149
+
+import csv
+import json
+
+# open the needed files
+csvfile = open('qol.csv', 'r')
+jsonfile = open('qol.json', 'w')
+
+# define fieldnames
+fieldnames = (
+        "Country",
+        "QualityofLifeIndex",
+        "PurchasingPowerIndex",
+        "SafetyIndex",
+        "HealthCareIndex",
+        "CostofLivingIndex",
+        "PropertyPricetoIncomeRatio",
+        "TrafficCommuteTimeIndex",
+        "PollutionIndex",
+        "ClimateIndex")
+
+reader = csv.DictReader(csvfile, fieldnames)
+
+# make array points
+points = []
+
+# store the data points in array points
+for row in reader:
+    points.append(row)
+
+# make dictionary for the json
+points_dict = {'data' : points}
+# make the json file
+json.dump(points_dict, jsonfile)
